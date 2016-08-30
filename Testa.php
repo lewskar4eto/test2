b725515a3b9534994b91013ffdb5bd8378000700
lewskar4eto <petar_98_@abv.bg>
2016-08-30 16:13:48 +0300

    Add files via upload
    Kolkoto tolkova, malko e no e ot syrce <3!!

---
+++ Testa.php
@@ -0,0 +1,41 @@
+<?php
+
+
+Interface Mammal {
+	public function  gettalk();
+	public function getwalk();
+	class Person 
+	{
+		protected $name;
+		protected $age;
+		protected $gender;
+	}
+	
+	class Animal
+	{
+		protected $name;
+		protected $age;
+		protected $gender;
+		
+	}
+	
+	class Dolphin  extends Animal
+	{
+		
+		function __construct(argument)
+		{
+			
+		}
+	}
+	
+	class Student extends Person
+	{
+		
+		function __construct(argument)
+		{
+			
+		}
+	}
+}
+
+?>
